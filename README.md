0375a381840bb7d75ef98abf42f3df6de4744ddb
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-06-25 08:17:23 +0000

    Add code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # expressapi
+Express Image Minifier
---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const multer  = require('multer')
+// Saves uploaded pics at uploads dir
+const upload = multer({ dest: 'uploads/' })
+const port = 3000
+
+async function begin() {
+	const {default: imagemin} = await import('imagemin')
+	const {default: imageminJpegtran} = await import('imagemin-jpegtran')
+	const {default: imageminPngquant} = await import('imagemin-pngquant')
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+app.post('/photo/upload', upload.single('pic'), async function (req, res, next) {
+      // Image Minifier with default values
+      await imagemin([`${req.file.path}`], {
+        destination: 'minified/',
+        plugins: [
+            imageminJpegtran(),
+            imageminPngquant({
+                quality: [0.6, 0.8]
+            })
+        ]
+    });
+      res.download(`minified/${req.file.filename}`, `minified-${req.file.originalname}`);
+  })
+  
+  
+  
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
+
+
+}
+
+begin()
+
